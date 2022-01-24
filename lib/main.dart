1aec8cbbc7b389e140c316a49da8f9c3ef0103e8
Rabindra <drarabin@gmail.com>
2022-01-24 20:33:36 +0545

    sign up ui and image picker

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:instagram_flutterclone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutterclone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_flutterclone/responsive/web_screen_layout.dart';
+import 'package:instagram_flutterclone/screens/signup_screen.dart';
 import 'package:instagram_flutterclone/utils/colors.dart';
 
 import 'screens/login_screen.dart';
@@ -43,7 +44,7 @@ class MyApp extends StatelessWidget {
       //   mobileScreenLayout: MobileScreenLayout(),
       //   webScreenLayout: WebScreenLayout(),
       // ),
-      home: LoginScreen(),
+      home: SignupScreen(),
     );
   }
 }
